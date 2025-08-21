17c2daa93999ede5384c12bc1facf9e373fdb167
Shresht5 <shreshtabalapure5@gmail.com>
2025-08-21 11:33:26 +0530

    21-8

--- day1/day1.cpp
+++ day1/day1.cpp
@@ -1,42 +1,133 @@
 #include <iostream>
 using namespace std;
 
-void show(); // prototyping
-
-class Animal // class
+class Person // class
 {
-private:
+    int id;
     string name;
+    string dateOfBirth;
 
 public:
-    Animal(string n)
+    Person(int _id = 1)
     {
-        name = n;
+        id = _id;
     }
-    void prints()
+
+    int getId()
     {
-        cout << name << " is Animal" << endl;
+        return id;
+    }
+    string getName()
+    {
+        return name;
+    }
+    string getDOB()
+    {
+        return dateOfBirth;
+    }
+    void setNAme(string _name)
+    {
+        name = _name;
+    }
+    void setDOB(string date)
+    {
+        dateOfBirth = date;
     }
 };
 
-class Dog : public Animal // inheritance
+class Student : public Person // inheritance
 {
-public:
-    Dog(string n) : Animal(n) {}
-    void speak() { cout << "bark!" << endl; }
-};
+    int Class;
+    char section;
+    bool fee;
 
+public:
+    Student(int _id = 1) : Person(_id)
+    {
+        fee = false;
+    }
+    void setClassSection(int _Class, char _section)
+    {
+        Class = _Class;
+        section = _section;
+    }
+    int getClass()
+    {
+        return Class;
+    }
+    char getSection()
+    {
+        return section;
+    }
+    void feePaid()
+    {
+        fee = true;
+    }
+    void studentDetails()
+    {
+        cout << "Id :" << getId() << "\nName :" << getName() << "\nDate Of Birth :" << getDOB() << "\nClass :" << Class << "\nSection :" << section << "\nfee paid :";
+        if (fee)
+        {
+            cout << "paid";
+        }
+        else
+        {
+            cout << "not paid";
+        }
+        cout << endl;
+    }
+};
+class Teacher : public Person // inheritance
+{
+    int mainClass;
+    char mainSection;
+    string subject;
+
+public:
+    Teacher(int _id = 1) : Person(_id)
+    {
+    }
+    void setClassSection(int _Class, char _section)
+    {
+        mainClass = _Class;
+        mainSection = _section;
+    }
+    int getClass()
+    {
+        return mainClass;
+    }
+    char getSection()
+    {
+        return mainSection;
+    }
+    void setSubject(string _subject)
+    {
+        subject = _subject;
+    }
+    void TeacherDetails()
+    {
+        cout << "Id :" << getId() << "\nName :" << getName() << "\nDate Of Birth :" << getDOB() << "\nClass :" << mainClass << "\nSection :" << mainSection << "\nsubject :" << subject << endl;
+    }
+};
+void underline();
 int main()
 {
-    cout << "hello" << endl;
-    Dog d("Goofy"); // object
-    d.prints();
-    d.speak();
-    show();
+    Student A1(1001);
+    A1.setNAme("Samarth");
+    A1.setDOB("12/5/1999");
+    A1.setClassSection(11, 'A');
+    A1.feePaid();
+    A1.studentDetails();
+    underline();
+    Teacher T1(101);
+    T1.setNAme("Kiran");
+    T1.setDOB("1/12/1971");
+    T1.setClassSection(11, 'A');
+    T1.setSubject("English");
+    T1.TeacherDetails();
     return 0;
 }
-
-void show()
+void underline()
 {
-    cout << "_____AI_____" << endl;
+    cout << "__________________________" << endl;
 }
--- day2/day2.cpp
+++ day2/day2.cpp
@@ -5,57 +5,108 @@ using namespace std;
 class Shape
 {
 protected:
-    int side1;
-    int side2; // encapsulation
-    int side3;
-
+    int side[3]; // encapsulation
 public:
-    Shape() { cout << "this  is shape" << endl; };
+    Shape(int S1) // polymorphism
+    {
+        side[0] = S1;
+    };
+    Shape(int S1, int S2)
+    {
+        side[0] = S1;
+        side[1] = S2;
+    };
+    Shape(int S1, int S2, int S3)
+    {
+        side[0] = S1;
+        side[1] = S2;
+        side[2] = S3;
+    };
     virtual void area() {};
 };
+//********************square class********************* */
 class Square : public Shape
 {
 public:
-    Square(int s1)
-    {
-        side1 = s1;
-    }
-    void area() override { cout << "area of square:" << side1 * side1 << endl; } // abstraction
+    Square(int S1) : Shape(S1) {};
+    void area() override { cout << "area of square :" << side[0] * side[0] << endl; }
 };
+//********************rectangle class********************* */
+
 class Rectangle : public Shape
 {
 public:
-    Rectangle(int s1, int s2) // polimorphism
-    {
-        side1 = s1;
-        side2 = s2;
-    }
-    void area() override { cout << "area of rectangle:" << side1 * side2 << endl; }
+    Rectangle(int S1, int S2) : Shape(S1, S2) {};
+
+    void area() override { cout << "area of rectangle :" << side[0] * side[1] << endl; }
 };
+//********************triangle class********************* */
+
 class Triangle : public Shape
 {
 public:
-    Triangle(int s1, int s2, int s3)
+    Triangle(int S1, int S2, int S3) : Shape(S1, S2, S3) {};
+
+    void area() override // abstraction hiding functionality and only show essential things
     {
-        side1 = s1;
-        side2 = s2;
-        side3 = s3;
-    }
-    void area() override
-    {
-        int s = (side1 + side2 + side3) / 2;
-        int area = sqrt(s * (s - side1) * (s - side2) * (s - side3));
-        cout << "area of triangle:" << area << endl;
+        int s = (side[0] + side[1] + side[2]) / 2;
+        int area = sqrt(s * (s - side[0]) * (s - side[1]) * (s - side[2]));
+        cout << "area of triangle :" << area << endl;
     }
 };
-
+void home()
+{
+    int choice;
+    cout << "choose shape that you want to find area\n1. square\n2. rectangle\n3. triangle\n";
+    cin >> choice;
+    if (choice == 1)
+    {
+        cout << "enter side length :";
+        int side1;
+        cin >> side1;
+        Square s(side1);
+        s.area();
+    }
+    else if (choice == 2)
+    {
+        cout << "enter side length :";
+        int side1;
+        cin >> side1;
+        cout << "enter side width :";
+        int side2;
+        cin >> side2;
+        Rectangle r(side1, side2);
+        r.area();
+    }
+    else if (choice == 3)
+    {
+        cout << "enter first side length :";
+        int side1;
+        cin >> side1;
+        cout << "enter second side length :";
+        int side2;
+        cin >> side2;
+        cout << "enter third side length :";
+        int side3;
+        cin >> side3;
+        Triangle t(side1, side2, side3);
+        t.area();
+    }
+    else
+    {
+        exit(0);
+    }
+}
 int main()
 {
-    Square s(5);
-    Rectangle r(4, 6);
-    Triangle t(3, 4, 5);
-    s.area();
-    r.area();
-    t.area();
+    // Square s(5);
+    // Rectangle r(4, 6);
+    // Triangle t(3, 4, 5);
+
+    // s.area();
+    // r.area();
+    // t.area();
+
+    home();
     return 0;
 }
